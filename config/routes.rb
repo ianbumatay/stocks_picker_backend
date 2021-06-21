0eccd39ee1db3cf28a5e787393d846ab8cfa3c6c
IAN BUMATAY <IANBUMATAY.09@GMAIL.COM>
2021-06-21 10:04:20 -0400

    created namespace for stocks_controller, folder and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
-  resources :stocks
+
+
+  namespace :api do
+    namespace :v1 do
+      resources :stocks 
+    end 
+  end
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
