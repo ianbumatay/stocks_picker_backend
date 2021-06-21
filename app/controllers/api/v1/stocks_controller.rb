b5f45226f3e8b39371f44551486314ddbc54a356
IAN BUMATAY <IANBUMATAY.09@GMAIL.COM>
2021-06-21 10:27:17 -0400

    created  actions in my StocksController

--- app/controllers/api/v1/stocks_controller.rb
+++ app/controllers/api/v1/stocks_controller.rb
@@ -1,2 +1,34 @@
 class Api::V1::StocksController < ApplicationController
+
+    def index 
+        stocks = Stock.all 
+        render json: stocks
+    end  
+
+    def create 
+        stock = Stock.new(stock_params)
+
+        if stock.save 
+            render json: stock 
+        else
+            render json: {message: "Stock data not found"} 
+        end
+    end  
+
+    def show 
+        stock = Stock.find(params[:id]) 
+        render json: stock 
+    end 
+
+    def destroy 
+        stock = Stock.find(params[:id])
+        stock.destroy 
+    end
+
+    private 
+
+    def stock_params 
+        params.require(:stock).permit(:name, :symbol, :price)
+    end
+
 end
